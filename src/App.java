5042ab5fa36e99222513cd135e871a4aa7d7f593
dishant b <code.dishb@gmail.com>
2024-11-06 10:19:58 -0800

    do stuff

--- src/App.java
+++ src/App.java
@@ -2,13 +2,30 @@ import java.util.*;
 
 public class App {
     public static Orders orders = new Orders();
-    public static Scanner strInput = new Scanner(System.in);
-    public static Scanner intInput = new Scanner(System.in);
+
+    public static Scanner sc = new Scanner(System.in);
+    public static String strInput = "";
+    public static int intInput = "";
 
     public static void main(String args[]) {
-        // Chatbot code here
+        String greeting = Greetings.getRandom() + " I am In-n-Out's chatbot. Can I take your order? (yes/no)";
+        System.out.println(greeting);
 
-        strInput.close();
-        intInput.close();
+        strInput = sc.nextLine();
+        if (contains({"y", "yes"}, strInput.toLowerCase())) {
+            // Take order
+        }
+        
+        sc.close()
+    }
+
+    public static boolean contains(String[] arr, String el) {
+        for (x : arr) {
+            if (x == el) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
