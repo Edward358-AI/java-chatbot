d5cc7090adb2926534df1a6d37ec11c9f086d066
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2024-11-30 01:27:12 +0000

    add extras to printer class and overload them as necessary

--- src/chatbot/Chatbot.java
+++ src/chatbot/Chatbot.java
@@ -126,7 +126,7 @@ No thanks
                 } else if (!keyword(resp, YES) && keyword(resp, NO)) {
                     tempState = noState;
                 } else {
-                    Printer.print(Apologies.getRandom() + customErrMsg);
+                    Printer.println(Apologies.getRandom() + customErrMsg);
                     tempState = null;
                 }
             }
@@ -135,7 +135,7 @@ No thanks
     }
 
     private void askQuestion() {
-        Printer.print("Do you have any questions regarding location, nutrition, or contact info, or would you like to view the orders? Please choose one.");
+        Printer.println("Do you have any questions regarding location, nutrition, or contact info, or would you like to view the orders? Please choose one.");
         String[] location = {"location"};
         String[] nutrition = {"nutrition"};
         String[] contact = {"contact"};
@@ -163,7 +163,7 @@ No thanks
                 }  else if (!choseLocation && !choseNutrition && !choseOrder && !choseContact && saidNo) {
                     tempState = "welcome";
                 } else {
-                    Printer.print(Apologies.getRandom() + " Would you like information regarding location, nutrition, or contact info? Or would you like to view the order queue?");
+                    Printer.println(Apologies.getRandom() + " Would you like information regarding location, nutrition, or contact info? Or would you like to view the order queue?");
                     continue;
                 }
             }
@@ -212,14 +212,14 @@ No thanks
     }
 
     private void welcome() {
-        Printer.print(String.format(
+        Printer.println(String.format(
                 "%s I am In 'n' Out's virtual assistant. If at any time you would like to stop chatting, just say \"quit\" or \"q\" anytime. Can I take your order today?",
                 Greetings.getRandom()));
         readYesNo("takeOrder", "askQuestion", " Can I take your order today?");
     }
 
     private void goodbye() {
-        Printer.print(String.format(
+        Printer.println(String.format(
                 "%s As the In 'n' Out chatbot, I don't necessarily deliver quality you can taste, but quality you can trust! Until next time!", Goodbyes.getRandom()));
     }
 
@@ -228,16 +228,16 @@ No thanks
         ArrayList<Double> orderPrices = new ArrayList<Double>();
         
         boolean finished = false;
-        Printer.print("Here is our menu:\n");
+        Printer.println("Here is our menu:\n");
         Menu.printMenu();
-        Printer.print();
+        Printer.println();
         do {
-            Printer.print("Please type out the name of an item you would like to add to your order. If you are finished type \"finished\".");
+            Printer.println("Please type out the name of an item you would like to add to your order. If you are finished type \"finished\".");
             String cItem = sc.nextLine().toLowerCase().trim();
             checkQuit(cItem);
             if (keyword(cItem, new String[]{"finished", "finish", "done", "end", "ready"})) {
                 if (orderItems.size() == 0) {
-                    System.out.print("You must enter at least one item. ");
+                    Printer.print("You must enter at least one item. ");
                     continue;
                 } else {
                     finished = true;
@@ -246,27 +246,27 @@ No thanks
                 if (Menu.getInfo(cItem) != null) {
                     orderItems.add(Utils.capitalize(cItem));
                     orderPrices.add(Menu.getInfo(cItem)[0]);
-                    Printer.print(Utils.capitalize(cItem) + " added to your order.");
+                    Printer.println(Utils.capitalize(cItem) + " added to your order.");
                 } else {
-                    Printer.print("Food item not found. Please check your spelling and try again. Thanks!");
+                    Printer.println("Food item not found. Please check your spelling and try again. Thanks!");
                     continue;
                 }
             }
         } while (!finished);
         viewOrder order = orders.saveOrder(orderItems, orderPrices);
-        Printer.print("Here are your order details: \n");
-        Printer.print(order.asText());
-        System.out.print("While you're waiting, ");
+        Printer.println("Here are your order details: \n");
+        Printer.println(order.asText());
+        Printer.print("While you're waiting, ");
         setState("askQuestion");
     }
 
     private void location() {
         boolean locFound = false;
         do {
-            Printer.print("Regarding location, what state would you like to get information about? You can enter either the name or postal code (the two letter abbreviation).");
+            Printer.println("Regarding location, what state would you like to get information about? You can enter either the name or postal code (the two letter abbreviation).");
             String state = sc.nextLine().toLowerCase().trim();
             checkQuit(state);
-            Printer.print("What city in that state would you like location information regarding?");
+            Printer.println("What city in that state would you like location information regarding?");
             String city = sc.nextLine().toLowerCase().trim();
             checkQuit(city);
             String[][] locations;
@@ -276,23 +276,23 @@ No thanks
                 locations = Locations.getLocationsByState(city, state);
             }
             if (locations != null) {
-                Printer.print("Here are the locations in the area that you specified.");
+                Printer.println("Here are the locations in the area that you specified.");
                 for (String[] location : locations) {
-                    Printer.print(location[2] + ", " + location[1] + ", " + location[3]);
+                    Printer.println(location[2] + ", " + location[1] + ", " + location[3]);
                 }
                 locFound = true;
             } else {
-                System.out.print(Apologies.getRandom()+" There were no locations found/the city and state were invalid. ");
+                Printer.print(Apologies.getRandom()+" There were no locations found/the city and state were invalid. ");
             }
         } while (!locFound);
-        System.out.print("Hopefully you found what you were looking for. ");
+        Printer.print("Hopefully you found what you were looking for. ");
         setState("askQuestion");
     }
 
     private void nutrition() {
         boolean nutrFound = false;
         do {
-            Printer.print("Regarding nutrition, what food item would you like to get information about? Type \"menu\" or \"m\" to see our menu.");
+            Printer.println("Regarding nutrition, what food item would you like to get information about? Type \"menu\" or \"m\" to see our menu.");
             String food = sc.nextLine().toLowerCase().trim();
             checkQuit(food);
             if (keyContains(food, new String[]{"menu", "m"})) {
@@ -301,23 +301,23 @@ No thanks
             }
             double[] nutrInfo = Menu.getInfo(food);
             if (nutrInfo != null) {
-                Printer.print(Utils.capitalize(food) + " has " + (int) nutrInfo[1] + " calories.");
+                Printer.println(Utils.capitalize(food) + " has " + (int) nutrInfo[1] + " calories.");
                 nutrFound = true;
             } else {
-                System.out.print(Apologies.getRandom() + " ");
+                Printer.print(Apologies.getRandom() + " ");
             }
         } while (!nutrFound);
-        System.out.print("Hopefully you found what you were looking for. ");
+        Printer.print("Hopefully you found what you were looking for. ");
         setState("askQuestion");
     }
 
     private void viewOrders() {
         if (orders.queueEmpty()) {
-            Printer.print("There are no orders to display.");
+            Printer.println("There are no orders to display.");
             setState("askQuestion");
         } else {
-            Printer.print("Here is the list of current orders:\n\n" + orders.asText());
-            Printer.print("Would you like to update any of these orders?");
+            Printer.println("Here is the list of current orders:\n\n" + orders.asText());
+            Printer.println("Would you like to update any of these orders?");
             readYesNo("updateOrder", "askQuestion", " Would you like to update an order?");
         }
     }
@@ -327,7 +327,7 @@ No thanks
         ArrayList<String> removed = new ArrayList<String>();
         int orderNum = 0;
         do {
-            Printer.print("Please input the order number of the order you would like to update.");
+            Printer.println("Please input the order number of the order you would like to update.");
             String s = sc.nextLine().trim();
             checkQuit(s);
             if (Utils.strIsInt(s) && orders.viewOrder(Integer.parseInt(s)) != null) {
@@ -337,7 +337,7 @@ No thanks
         
         boolean finished = false;
         do {
-            Printer.print("Please enter an item in you order you would like to remove. Type \"menu\" or \"m\" anytime to view the menu. When finished, type \"finished\"");
+            Printer.println("Please enter an item in you order you would like to remove. Type \"menu\" or \"m\" anytime to view the menu. When finished, type \"finished\"");
             String food = sc.nextLine().toLowerCase().trim();
             checkQuit(food);
             if (keyContains(food, new String[]{"menu", "m"})) {
@@ -346,7 +346,7 @@ No thanks
             }
             if (keyword(food, new String[]{"finished", "finish", "done", "end", "ready"})) {
                 if (removed.size() == 0) {
-                    System.out.print("No items were removed from order.");
+                    Printer.print("No items were removed from order.");
                     finished = true;
                 } else {
                     finished = true;
@@ -356,20 +356,20 @@ No thanks
                     food = Utils.capitalize(food);
                     orders.removeItem(orderNum, food);
                     removed.add(food);
-                    Printer.print(food + " was removed from order #" + orderNum + ".");
+                    Printer.println(food + " was removed from order #" + orderNum + ".");
                     continue;
                 } else {
-                    Printer.print("Food item not found/not in order. Please check your spelling and try again. Thanks!");
+                    Printer.println("Food item not found/not in order. Please check your spelling and try again. Thanks!");
                     continue;
                 }
             }
         } while (!finished);
 
-        Printer.print();
+        Printer.println();
         finished = false;
 
         do {
-            Printer.print("Please enter an item in you order you would like to add. Type \"menu\" or \"m\" anytime to view the menu. When finished, type \"finished\"");
+            Printer.println("Please enter an item in you order you would like to add. Type \"menu\" or \"m\" anytime to view the menu. When finished, type \"finished\"");
             String food = sc.nextLine().toLowerCase().trim();
             checkQuit(food);
             if (keyContains(food, new String[]{"menu", "m"})) {
@@ -378,7 +378,7 @@ No thanks
             }
             if (keyword(food, new String[]{"finished", "finish", "done", "end", "ready"})) {
                 if (added.size() == 0) {
-                    System.out.print("No items were added to order.");
+                    Printer.print("No items were added to order.");
                     finished = true;
                 } else {
                     finished = true;
@@ -388,25 +388,25 @@ No thanks
                     food = Utils.capitalize(food);
                     orders.addItem(orderNum, food);
                     added.add(food);
-                    Printer.print(food + " was added to order #" + orderNum + ".");
+                    Printer.println(food + " was added to order #" + orderNum + ".");
                     continue;
                 } else {
-                    Printer.print("Food item not found. Please check your spelling and try again. Thanks!");
+                    Printer.println("Food item not found. Please check your spelling and try again. Thanks!");
                     continue;
                 }
             }
         } while (!finished);
         viewOrder order = orders.viewOrder(orderNum);
         String isNew = (added.size() > 0 || removed.size() > 0) ? "new" : "";
-        Printer.print("Here are your " + isNew + " order details: \n");
-        Printer.print(order.asText());
-        System.out.print("Now that your order has been updated, ");
+        Printer.println("Here are your " + isNew + " order details: \n");
+        Printer.println(order.asText());
+        Printer.print("Now that your order has been updated, ");
         setState("askQuestion");
     }
 
     private void contactInfo() {
-        Printer.print("\nIf you have any questions, please go to https://in-n-out.com/contact if you have any particular questions, comments, and concerns.\nWe are also available by phone, you can dial an associate at 1-800-786-1000. Our office hours are:\nSunday to Thursday: 8am - 1am\nFriday to Saturday: 8am to 1:30am\nYou can also write directly to customer service, here is our mailbox:\nIn-N-Out Burgers Corporate Office\n4199 Campus Drive, 9th Floor\nIrvine, CA 92612\n");
-        System.out.print("Now that's out of the way, ");
+        Printer.println("\nIf you have any questions, please go to https://in-n-out.com/contact if you have any particular questions, comments, and concerns.\nWe are also available by phone, you can dial an associate at 1-800-786-1000. Our office hours are:\nSunday to Thursday: 8am - 1am\nFriday to Saturday: 8am to 1:30am\nYou can also write directly to customer service, here is our mailbox:\nIn-N-Out Burgers Corporate Office\n4199 Campus Drive, 9th Floor\nIrvine, CA 92612\n");
+        Printer.print("Now that's out of the way, ");
         setState("askQuestion");
     }
 
