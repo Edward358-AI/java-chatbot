12f14082b5819584eb7b7b168e1a1156b26de34a
Edward Jiang <66029887+Edward358-AI@users.noreply.github.com>
2024-11-30 04:59:25 +0000

    fix printer.print in menu.java file

--- src/data/Menu.java
+++ src/data/Menu.java
@@ -40,24 +40,24 @@ public class Menu {
     }
 
     public static void printMenu() {
-        Printer.print("Food items:", Colors.CYAN_FG);
+        Printer.println("Food items:", Colors.CYAN_FG);
         for (int i = 0; i < Food.ITEMS.length; i++) {
-            Printer.print(Food.ITEMS[i] + ": $" + Colors.GREEN_FG + Food.PRICES[i]);
+            Printer.println(Food.ITEMS[i] + ": $" + Colors.GREEN_FG + Food.PRICES[i]);
         }
 
-        Printer.print("\nDrinks:", Colors.CYAN_FG);
+        Printer.println("\nDrinks:", Colors.CYAN_FG);
         for (int i = 0; i < Drinks.ITEMS.length; i++) {
-            Printer.print(Drinks.ITEMS[i] + ": $" + Colors.GREEN_FG + Drinks.PRICES[i]);
+            Printer.println(Drinks.ITEMS[i] + ": $" + Colors.GREEN_FG + Drinks.PRICES[i]);
         }
 
-        Printer.print("\nShakes:", Colors.CYAN_FG);
+        Printer.println("\nShakes:", Colors.CYAN_FG);
         for (int i = 0; i < Shakes.ITEMS.length; i++) {
-            Printer.print(Shakes.ITEMS[i] + ": $" + Colors.GREEN_FG + Shakes.PRICES[i]);
+            Printer.println(Shakes.ITEMS[i] + ": $" + Colors.GREEN_FG + Shakes.PRICES[i]);
         }
 
-        Printer.print("\n(Not so) Secret Menu:", Colors.CYAN_FG);
+        Printer.println("\n(Not so) Secret Menu:", Colors.CYAN_FG);
         for (int i = 0; i < Secret.ITEMS.length; i++) {
-            Printer.print(Secret.ITEMS[i] + ": $" + Colors.GREEN_FG + Secret.PRICES[i]);
+            Printer.println(Secret.ITEMS[i] + ": $" + Colors.GREEN_FG + Secret.PRICES[i]);
         }
     }
 }
