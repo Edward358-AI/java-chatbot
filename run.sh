9d9a42821792b2db950edd02dba7e550dfcca987
dishb <code.dishb@gmail.com>
2024-12-04 08:57:59 -0800

    add run script for windows

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/zsh
 
-# Check if the OS is macOS or UNIX-based
 os_type=$(uname)
 
 if [[ "$os_type" == "Darwin" ]]; then
@@ -10,20 +9,14 @@ else
     exit 1
 fi
 
-# Navigate to the script's directory (relative to script location)
 cd "$(dirname "$0")"
 
-# Ensure the bin directory exists
 if [ ! -d "./bin" ]; then
     mkdir -p ./bin
 fi
 
-# Remove old compiled files
 rm -rf ./bin/*
 
-# Enable globbing for recursive globbing in zsh (zsh supports globstar natively)
-# Use zsh's ** globbing to match all .java files in the src directory recursively
 javac -d ./bin ./src/**/*.java
 
-# Run the Java application
 java --enable-preview -cp ./bin App
